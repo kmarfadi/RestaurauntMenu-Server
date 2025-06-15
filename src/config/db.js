12dcd1acf62cd1ae97118f017615f72070219c49
Khalifah Marfadi <marf@Khalifahs-MacBook-Pro.local>
2025-06-15 21:10:58 +0300

    added some comments for better readability

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,13 @@ dotenv.config();
 // CERTIFICATE
 
 // PostgreSQL setup
+// DB currently uses SSL, so we need to set up the connection accordingly
+// DB is hosted on AIVEN at the moment
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true,}
+    rejectUnauthorized: true,// Set to false if you don't want to reject unauthorized certificates
+  }
 });
 
 module.exports = { pool };
